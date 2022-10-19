e17d79863abda8c1fe6e113986fd600cbfe39bf6
jking412 <3250532948@qq.com>
2022-10-19 11:07:33 +0800

    update config.yaml

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"io"
 	"log"
 	"os"
@@ -10,7 +11,7 @@ import (
 func InitLogger() {
 	logrus.SetReportCaller(true)
 	stdoutWriter := os.Stdout
-	FileWriter, err := os.OpenFile("logs/logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	FileWriter, err := os.OpenFile(viper.GetString("log.path"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Print("Failed to open log file")
 	}
