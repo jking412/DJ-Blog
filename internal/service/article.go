d9fa7ffd5aa76414d696c7d11f03abfdf8c4b200
jking412 <3250532948@qq.com>
2022-11-16 15:40:56 +0800

    service初步

--- internal/service/article.go
+++ internal/service/article.go
@@ -1,6 +1,10 @@
 package service
 
-import "DJ-Blog/internal/model"
+import (
+	"DJ-Blog/internal/model"
+	"DJ-Blog/pkg/database"
+	"github.com/sirupsen/logrus"
+)
 
 type ArticleService struct {
 }
@@ -13,4 +17,31 @@ type Article struct {
 
 func NewArticleService() *ArticleService {
 	return new(ArticleService)
+}
+
+func (a *ArticleService) GetArticleList() ([]Article, bool) {
+	articles := make([]Article, 0)
+	if err := database.DB.Model(&model.ArticleModel{}).Find(articles).Error; err != nil {
+		logrus.Warn("Get article list failed", err)
+		return nil, false
+	}
+	return articles, true
+}
+
+func (a *ArticleService) GetArticleById(id uint32) (*Article, bool) {
+	article := &Article{}
+	if err := database.DB.Model(&model.ArticleModel{}).Where("id = ?", id).First(article).Error; err != nil {
+		logrus.Warn("Get article failed", err)
+		return nil, false
+	}
+	return article, true
+}
+
+func (a *ArticleService) GetArticleByTitle(title string) (*Article, bool) {
+	article := &Article{}
+	if err := database.DB.Model(&model.ArticleModel{}).Where("title = ?", title).First(&article).Error; err != nil {
+		logrus.Warn("Get article failed", err)
+		return nil, false
+	}
+	return article, true
 }
--- internal/service/user.go
+++ internal/service/user.go
@@ -1,8 +1,33 @@
 package service
 
+import (
+	"DJ-Blog/internal/model"
+	"DJ-Blog/pkg/database"
+)
+
 type UserService struct {
 }
 
 func NewUserService() *UserService {
 	return new(UserService)
+}
+
+type User struct {
+	model.UserModel
+}
+
+func (u *UserService) GetUserById(id int32) (*User, bool) {
+	user := &User{}
+	if err := database.DB.Model(&model.UserModel{}).Where("id = ?", id).First(user).Error; err != nil {
+		return nil, false
+	}
+	return user, true
+}
+
+func (u *UserService) GetUserByUsername(username string) (*User, bool) {
+	user := &User{}
+	if err := database.DB.Model(&model.UserModel{}).Where("username = ?", username).First(user).Error; err != nil {
+		return nil, false
+	}
+	return user, true
 }
